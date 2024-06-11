0edfd549598f33e881dbc54367400ddbc55a95b9
SirImperivm <134490705+SirImperivm@users.noreply.github.com>
2024-06-11 22:18:08 +0200

    Changelog: - Inserimento sistema di logging; - Inserimento sistema di configurazione; - Inserimento classe errori.
    Aggiornamenti Previsti:
    - Inserimento sistema di funzioni base;
    - Inserimento comandi e eventi.

--- src/main/java/me/sirimperivm/spigot/Main.java
+++ src/main/java/me/sirimperivm/spigot/Main.java
@@ -1,6 +1,9 @@
 package me.sirimperivm.spigot;
 
+import me.sirimperivm.spigot.utils.ConfigManager;
 import me.sirimperivm.spigot.utils.colors.Colors;
+import me.sirimperivm.spigot.utils.other.Errors;
+import me.sirimperivm.spigot.utils.other.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @SuppressWarnings("all")
@@ -8,16 +11,24 @@ public final class Main extends JavaPlugin {
     
     private Main plugin;
     private Colors colors;
+    private Logger log;
+    private ConfigManager configManager;
+    private Errors errors;
 
     @Override
     public void onEnable() {
         plugin = this;
         colors = new Colors(plugin);
+        log = new Logger(plugin, "ChatChannels");
+        configManager = new ConfigManager(plugin);
+        errors = new Errors(plugin);
+
+        log.success("Plugin attivato correttamente!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        log.success("Plugin disattivato correttamente!");
     }
 
     public Main getPlugin() {
@@ -26,5 +37,17 @@ public final class Main extends JavaPlugin {
 
     public Colors getColors() {
         return colors;
+    }
+
+    public Logger getLog() {
+        return log;
+    }
+
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+
+    public Errors getErrors() {
+        return errors;
     }
 }
